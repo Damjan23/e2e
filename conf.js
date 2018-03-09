b33730824b36ffc2ee75739ddc770583230bf535
damjan_smickovski <damjan.smickovski@alite-international.com>
2018-03-09 14:45:36 +0100

    test

--- conf.js
+++ conf.js
@@ -1,7 +1,16 @@
 exports.config = {
   // set to "custom" instead of cucumber.
   framework: 'custom',
-
+  
+ directConnect: false,
+ 
+  maxSessions: 1,  // 6 TOTAL sessions across all capabilities
+  multiCapabilities: [{ // in 1 chrome run the 10 specs sequentially
+    browserName: 'chrome',
+  //  client_key: "70840f7b58dbca197b5bce786ebc3771",
+ //   client_secret: "b0298d440b539a5d1dbef0c95c7998c6"
+  }],
+  
   // path relative to the current config file
   frameworkPath: require.resolve('protractor-cucumber-framework'),
 
